c5baeb0e673464ae7b05162b69e65291ed049c12
MatchaEggTart <MatchaEggTart@hotmail.com>
2025-01-22 01:20:50 +0800

    update

--- config/emacs/elpa/quickrun-2.3.1/quickrun-pkg.el
+++
@@ -1,9 +0,0 @@
-;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "quickrun" "2.3.1"
-  "Run commands quickly."
-  '((emacs "24.3"))
-  :url "https://github.com/syohex/emacs-quickrun"
-  :commit "0133cac651918071a824e85b55e10d44f32a1979"
-  :revdesc "0133cac65191"
-  :authors '(("Syohei YOSHIDA" . "syohex@gmail.com"))
-  :maintainers '(("Syohei YOSHIDA" . "syohex@gmail.com")))
