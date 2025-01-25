544bc83f7d52672669d5bf990a59f76348631f16
MatchaEggTart <MatchaEggTart@hotmail.com>
2025-01-26 00:46:58 +0800

    update

--- hypr_install.sh
+++ hypr_install.sh
@@ -5,7 +5,7 @@ mkdir -p $HOME/Pictures/Wallpapers
 # if use input
 # sudo usermod -aG input matchaeggtart
 # 双显示器工具 https://wiki.archlinuxcn.org/wiki/%E8%83%8C%E5%85%89
-sudo pacman -Syy ddcutil
+paru -Syy ddccontrol
 # 可能需要安装
 # paru -Syy ddcci-driver-linux-dkms
 # 使用指令 ddcutil setvcp 10 70 设置为 70
