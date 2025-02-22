eacc570f22a9780a1fcfa312bacfad6987798ea7
MatchaEggTart <MatchaEggTart@hotmail.com>
2025-02-23 02:08:38 +0800

    update

--- hypr/scripts/screen_shot.sh
+++ hypr/scripts/screen_shot.sh
@@ -2,5 +2,5 @@
 FILE_PATH="$HOME/Pictures/Screenshots/screenshot-$(date +%s).png"
 grim -g "$(slurp)" "$FILE_PATH"
 wl-copy < "$FILE_PATH"
-swayimg --scale=fit "$FILE_PATH" &
+swayimg --scale=fit --config="info.show=no" "$FILE_PATH" &
 # 单击关闭窗口（需提前赋予权限：chmod +x ~/.local/bin/snipaste_swayimg）
