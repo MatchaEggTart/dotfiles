21308eedf8039ea8d7d380bcaf3c519e098b1102
MatchaEggTart <MatchaEggTart@hotmail.com>
2025-12-03 14:48:48 +0800

    Update

--- matchaeggtart/config/emacs/install_language_server.sh
+++ matchaeggtart/config/emacs/install_language_server.sh
@@ -1,4 +1,5 @@
-sudo pacman -S clang
+# C && Python
+sudo pacman -S clang python-lsp-server
 # npm install -D -g typescript typescript-language-server@4.0.0
 npm i -g vscode-langservers-extracted prettier typescript-language-server typescript eslint vscode-json-languageservice yaml-language-server @tailwindcss/language-server bash-language-server
 # sudo pacman -S tree-sitter
