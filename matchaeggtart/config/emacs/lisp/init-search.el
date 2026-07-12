001ae6187089a6b79b95c53311592d3c9a4b35ab
MatchaEggTart <MatchaEggTart@hotmail.com>
2026-07-13 03:14:43 +0800

    Update

--- matchaeggtart/config/emacs/lisp/init-search.el
+++ matchaeggtart/config/emacs/lisp/init-search.el
@@ -23,15 +23,15 @@
   :ensure nil
   :hook (after-init . savehist-mode)
   :init (setq enable-recursive-minibuffers t ; Allow commands in minibuffers
-	        history-length 1000
-	        savehist-additional-variables '(
+ 	        history-length 1000
+ 	        savehist-additional-variables '(
                                            mark-ring
-					                                 global-mark-ring
-					                                 search-ring
-					                                 regexp-search-ring
-					                                 extended-command-history
+ 					                                 global-mark-ring
+ 					                                 search-ring
+ 					                                 regexp-search-ring
+ 					                                 extended-command-history
                                            )
-	        savehist-autosave-interval 300)
+ 	        savehist-autosave-interval 300)
   )
 
 ;; 模糊搜索
@@ -42,17 +42,16 @@
   ;; (setq orderless-style-dispatchers '(+orderless-dispatch)
   ;;       orderless-component-separator #'orderless-escapable-split-on-space)
   (setq completion-styles '(orderless basic)
-	  completion-category-defaults nil
-	  completion-category-overrides '((file (styles partial-completion))))
+ 	  completion-category-defaults nil
+ 	  completion-category-overrides '((file (styles partial-completion))))
   )
-
 ;; 增强 minibuffer， 可以显示各种值跟其他数据
 ;; Enable rich annotations using the Marginalia package
 (use-package marginalia
   ;; Either bind `marginalia-cycle' globally or only in the minibuffer
   :bind (("M-A" . marginalia-cycle)
-	        :map minibuffer-local-map
-	        ("M-A" . marginalia-cycle))
+ 	        :map minibuffer-local-map
+ 	        ("M-A" . marginalia-cycle))
 
   ;; The :init configuration is always executed (Not lazy!)
   :init
@@ -61,6 +60,64 @@
   ;; enabled right away. Note that this forces loading the package.
   (marginalia-mode)
   )
+
+;; ;; Enable Vertico.
+;; (use-package vertico
+;;   ;; :custom
+;;   ;; (vertico-scroll-margin 0) ;; Different scroll margin
+;;   ;; (vertico-count 20) ;; Show more candidates
+;;   ;; (vertico-resize t) ;; Grow and shrink the Vertico minibuffer
+;;   ;; (vertico-cycle t) ;; Enable cycling for `vertico-next/previous'
+;;   :init
+;;   (vertico-mode))
+;;
+;; ;; Persist history over Emacs restarts. Vertico sorts by history position.
+;; (use-package savehist
+;;   :init
+;;   (savehist-mode))
+;;
+;; ;; Emacs minibuffer configurations.
+;; (use-package emacs
+;;   :custom
+;;   ;; Enable context menu. `vertico-multiform-mode' adds a menu in the minibuffer
+;;   ;; to switch display modes.
+;;   (context-menu-mode t)
+;;   ;; Support opening new minibuffers from inside existing minibuffers.
+;;   (enable-recursive-minibuffers t)
+;;   ;; Hide commands in M-x which do not work in the current mode.  Vertico
+;;   ;; commands are hidden in normal buffers. This setting is useful beyond
+;;   ;; Vertico.
+;;   (read-extended-command-predicate #'command-completion-default-include-p)
+;;   ;; Do not allow the cursor in the minibuffer prompt
+;;   (minibuffer-prompt-properties
+;;     '(read-only t cursor-intangible t face minibuffer-prompt)))
+;;
+;; ;; Optionally use the `orderless' completion style.
+;; (use-package orderless
+;;   :custom
+;;   ;; Configure a custom style dispatcher (see the Consult wiki)
+;;   ;; (orderless-style-dispatchers '(+orderless-consult-dispatch orderless-affix-dispatch))
+;;   ;; (orderless-component-separator #'orderless-escapable-split-on-space)
+;;   (completion-styles '(orderless basic))
+;;   (completion-category-overrides '((file (styles partial-completion))))
+;;   (completion-category-defaults nil) ;; Disable defaults, use our settings
+;;   (completion-pcm-leading-wildcard t)) ;; Emacs 31: partial-completion behaves like substring
+;;
+;; ;; Enable rich annotations using the Marginalia package
+;; (use-package marginalia
+;;   ;; Bind `marginalia-cycle' locally in the minibuffer.  To make the binding
+;;   ;; available in the *Completions* buffer, add it to the
+;;   ;; `completion-list-mode-map'.
+;;   :bind (:map minibuffer-local-map
+;;          ("M-A" . marginalia-cycle))
+;;
+;;   ;; The :init section is always executed.
+;;   :init
+;;
+;;   ;; Marginalia must be activated in the :init section of use-package such that
+;;   ;; the mode gets enabled right away. Note that this forces loading the
+;;   ;; package.
+;;   (marginalia-mode))
 
 ;; C-s C-r 搜索增强
 ;; sudo dnf install ripgrep || sudo pacman -S ripgrep
@@ -155,7 +212,8 @@
 ;; 然后 M-x，选择 query-replace-regrep(C-M-%)， 进行递归替换
 ;; 最后 C-c C-c 保存
 
-
+;; (advice-remove 'marginalia--time-relative '+vertico--marginalia-time-relative-a)
+;; 注意：请将 +vertico--marginalia-time-relative-a 替换为你在 C-h f 中看到的实际 advice 名称
 
 ;;; init-search.el ends here
 
